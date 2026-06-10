72df5da75449129ec949069e4b0d9466c0523883
kauehenrick <kauehenrick@proton.me>
2026-06-09 23:13:31 -0300

    feat: user register

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { Navigate, useNavigate } from "react-router";
 import type { z } from "zod";
@@ -6,112 +7,212 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { loginFormSchema, useAuthStore } from "@/stores/AuthStore";
+import {
+  loginFormSchema,
+  registerFormSchema,
+  useAuthStore,
+} from "@/stores/AuthStore";
 import logoFuXiImg from "../../assets/fuxi-logo.svg";
 
 export default function LoginPage() {
-	const { login, user } = useAuthStore();
-	const navigate = useNavigate();
+  const [isRegister, setIsRegister] = useState(false);
+  const { register, login, user } = useAuthStore();
+  const navigate = useNavigate();
 
-	const form = useForm<z.infer<typeof loginFormSchema>>({
-		resolver: zodResolver(loginFormSchema),
-		defaultValues: {
-			email: "",
-			password: "",
-		},
-	});
+  const form = useForm<
+    z.infer<typeof loginFormSchema | typeof registerFormSchema>
+  >({
+    resolver: zodResolver(isRegister ? registerFormSchema : loginFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
-	if (user) {
-		return <Navigate to="/" replace />;
-	}
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
 
-	async function onSubmit(values: z.infer<typeof loginFormSchema>) {
-		try {
-			await login(values);
+  const handleRegister = async (values: z.infer<typeof registerFormSchema>) => {
+    try {
+      await register(values);
 
-			form.reset();
-			navigate("/");
-		} catch (error) {
-			console.error(error);
-		}
-	}
+      form.reset();
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	return (
-		<div className="flex h-screen w-full items-center justify-center">
-			<Card className="w-full max-w-4xl p-10">
-				<img className="w-30" src={logoFuXiImg} alt="Logo do FuXi" />
+  const handleLogin = async (values: z.infer<typeof loginFormSchema>) => {
+    try {
+      await login(values);
 
-				<CardContent className="flex justify-between px-0">
-					<div>
-						<h1 className="font-medium text-3xl">Faça login na sua conta</h1>
-						<p className="mt-2 font-light">Entre usando suas credenciais</p>
-					</div>
+      form.reset();
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-					<div className="w-2/5">
-						<form
-							onSubmit={form.handleSubmit(onSubmit, (errors) => {
-								console.error("ERROS:", errors);
-							})}
-						>
-							<div className="flex flex-col gap-6">
-								<Controller
-									name="email"
-									control={form.control}
-									render={({ field, fieldState }) => (
-										<Field
-											data-invalid={fieldState.invalid}
-											className="max-w-full"
-										>
-											<FieldLabel htmlFor="title">Email </FieldLabel>
-											<Input
-												{...field}
-												id="email"
-												type="email"
-												aria-invalid={fieldState.invalid}
-												placeholder="fuxi@example.com"
-												autoComplete="on"
-											/>
-											{fieldState.invalid && (
-												<FieldError errors={[fieldState.error]} />
-											)}
-										</Field>
-									)}
-								/>
+  // async function onSubmit(values: z.infer<typeof loginFormSchema>) {
+  //   try {
+  //     await login(values);
 
-								<Controller
-									name="password"
-									control={form.control}
-									render={({ field, fieldState }) => (
-										<Field
-											data-invalid={fieldState.invalid}
-											className="max-w-full"
-										>
-											<FieldLabel htmlFor="title">Senha </FieldLabel>
-											<Input
-												{...field}
-												id="password"
-												type="password"
-												aria-invalid={fieldState.invalid}
-												placeholder="Insira sua senha."
-											/>
-											{fieldState.invalid && (
-												<FieldError errors={[fieldState.error]} />
-											)}
-										</Field>
-									)}
-								/>
-							</div>
+  //     form.reset();
+  //     navigate("/");
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
-							<footer className="mt-6 flex justify-end gap-2">
-								<Button variant="ghost" disabled>
-									Criar conta
-								</Button>
-								<Button type="submit">Entrar</Button>
-							</footer>
-						</form>
-					</div>
-				</CardContent>
-			</Card>
-		</div>
-	);
+  const onSubmit = isRegister ? handleRegister : handleLogin;
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <Card className="w-full max-w-4xl p-10">
+        <img className="w-30" src={logoFuXiImg} alt="Logo do FuXi" />
+
+        <CardContent className="flex justify-between px-0">
+          <div>
+            <h1 className="font-medium text-3xl">
+              {isRegister ? "Crie sua conta" : "Faça login na sua conta"}
+            </h1>
+
+            <p className="mt-2 font-light">
+              {isRegister
+                ? "Preencha os dados para continuar"
+                : "Entre usando suas credenciais"}
+            </p>
+          </div>
+
+          <div className="w-2/5">
+            <form
+              onSubmit={form.handleSubmit(onSubmit, (errors) => {
+                console.error("ERROS:", errors);
+              })}
+            >
+              <div className="flex flex-col gap-6">
+                {isRegister && (
+                  <Controller
+                    name="name"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Field
+                        data-invalid={fieldState.invalid}
+                        className="max-w-full"
+                      >
+                        <FieldLabel htmlFor="name">Nome</FieldLabel>
+
+                        <Input
+                          {...field}
+                          id="name"
+                          aria-invalid={fieldState.invalid}
+                          placeholder="Seu nome"
+                        />
+
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
+                )}
+
+                <Controller
+                  name="email"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      data-invalid={fieldState.invalid}
+                      className="max-w-full"
+                    >
+                      <FieldLabel htmlFor="title">Email </FieldLabel>
+                      <Input
+                        {...field}
+                        id="email"
+                        type="email"
+                        aria-invalid={fieldState.invalid}
+                        placeholder="fuxi@example.com"
+                        autoComplete="on"
+                      />
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
+                />
+
+                <Controller
+                  name="password"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      data-invalid={fieldState.invalid}
+                      className="max-w-full"
+                    >
+                      <FieldLabel htmlFor="title">Senha </FieldLabel>
+                      <Input
+                        {...field}
+                        id="password"
+                        type="password"
+                        aria-invalid={fieldState.invalid}
+                        placeholder="Insira sua senha."
+                      />
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
+                />
+
+                {isRegister && (
+                  <Controller
+                    name="confirmPassword"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Field
+                        data-invalid={fieldState.invalid}
+                        className="max-w-full"
+                      >
+                        <FieldLabel htmlFor="title">
+                          Confirme a senha{" "}
+                        </FieldLabel>
+                        <Input
+                          {...field}
+                          id="confirmPassword"
+                          type="password"
+                          aria-invalid={fieldState.invalid}
+                          placeholder="Confirme sua senha."
+                        />
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
+                )}
+              </div>
+
+              <footer className="mt-6 flex justify-end gap-2">
+                <Button
+                  type="button"
+                  variant="ghost"
+                  onClick={() => setIsRegister((prev) => !prev)}
+                >
+                  {isRegister ? "Já tenho conta" : "Criar conta"}
+                </Button>
+
+                <Button type="submit">
+                  {isRegister ? "Cadastrar" : "Entrar"}
+                </Button>
+              </footer>
+            </form>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -5,142 +5,204 @@ import { create } from "zustand";
 import { api } from "../services/api";
 
 export const loginFormSchema = z.object({
-	email: z.email({ message: "Informe um email válido." }),
-	password: z.string().min(6, {
-		message: "A senha deve ter no mínimo 6 caracteres.",
-	}),
+  email: z.email({ message: "Informe um email válido." }),
+  password: z.string().min(6, {
+    message: "A senha deve ter no mínimo 6 caracteres.",
+  }),
 });
 
+export const registerFormSchema = z
+  .object({
+    name: z.string().min(1, {
+      message: "O nome deve ser preenchido",
+    }),
+    email: z.email({
+      message: "Informe um email válido.",
+    }),
+    password: z.string().min(6, {
+      message: "A senha deve ter no mínimo 6 caracteres.",
+    }),
+    confirmPassword: z.string().min(6, {
+      message: "A senha deve ter no mínimo 6 caracteres.",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "As senhas não coincidem.",
+  });
+
 export const userSchema = z.object({
-	id: z.number(),
-	name: z.string(),
-	email: z.email(),
+  id: z.number(),
+  name: z.string(),
+  email: z.email(),
 });
 
 export type LoginFormProps = z.infer<typeof loginFormSchema>;
+export type RegisterFormProps = z.infer<typeof registerFormSchema>;
 export type UserProps = z.infer<typeof userSchema>;
 
 type AuthStoreProps = {
-	user: UserProps | null;
-	error: null | string | unknown;
-	loading: boolean;
-	initialized: boolean;
+  user: UserProps | null;
+  error: null | string | unknown;
+  loading: boolean;
+  initialized: boolean;
 
-	login: (credentials: LoginFormProps) => Promise<void>;
-	logout: () => Promise<void>;
-	getMe: () => Promise<void>;
+  register: (credentials: RegisterFormProps) => Promise<void>;
+  login: (credentials: LoginFormProps) => Promise<void>;
+  logout: () => Promise<void>;
+  getMe: () => Promise<void>;
 };
 
 export const useAuthStore = create<AuthStoreProps>((set) => ({
-	user: null,
-	error: null,
-	loading: false,
-	initialized: false,
+  user: null,
+  error: null,
+  loading: false,
+  initialized: false,
 
-	login: async (credentials) => {
-		try {
-			set({
-				error: null,
-				loading: true,
-			});
+  register: async (credentials) => {
+    try {
+      set({
+        error: null,
+        loading: true,
+      });
 
-			const { data } = await api.post("/login", credentials);
-			localStorage.setItem("token", data.token);
+      const { data } = await api.post("/users", credentials);
+      localStorage.setItem("token", data.token);
 
-			set({
-				user: data.user,
-			});
+      set({
+        user: data.user,
+      });
 
-			toast.success("Login realizado com sucesso.");
-		} catch (err) {
-			const error = err as AxiosError<{
-				message?: string;
-				errors?: Record<string, string[]>;
-			}>;
+      toast.success("Usuário cadastrado com sucesso.");
+    } catch (err) {
+      const error = err as AxiosError<{
+        message?: string;
+        errors?: Record<string, string[]>;
+      }>;
 
-			const message =
-				error?.response?.data?.message || "Erro inesperado ao realizar login.";
+      const message =
+        error?.response?.data?.message ||
+        "Erro inesperado ao cadastrar usuário.";
 
-			toast.error(message);
+      toast.error(message);
 
-			set({
-				error: err,
-				user: null,
-			});
+      set({
+        error: err,
+        user: null,
+      });
 
-			throw err;
-		} finally {
-			set({
-				loading: false,
-			});
-		}
-	},
+      throw err;
+    } finally {
+      set({
+        loading: false,
+      });
+    }
+  },
 
-	logout: async () => {
-		try {
-			set({
-				error: null,
-				loading: true,
-			});
+  login: async (credentials) => {
+    try {
+      set({
+        error: null,
+        loading: true,
+      });
 
-			await api.post("/logout");
+      const { data } = await api.post("/login", credentials);
+      localStorage.setItem("token", data.token);
 
-			set({
-				user: null,
-			});
+      set({
+        user: data.user,
+      });
 
-			toast.success("Logout realizado com sucesso.");
-		} catch (err) {
-			const error = err as AxiosError<{
-				message?: string;
-			}>;
+      toast.success("Login realizado com sucesso.");
+    } catch (err) {
+      const error = err as AxiosError<{
+        message?: string;
+        errors?: Record<string, string[]>;
+      }>;
 
-			const message =
-				error?.response?.data?.message || "Erro inesperado ao realizar logout.";
+      const message =
+        error?.response?.data?.message || "Erro inesperado ao realizar login.";
 
-			toast.error(message);
+      toast.error(message);
 
-			set({
-				error: err,
-			});
+      set({
+        error: err,
+        user: null,
+      });
 
-			throw err;
-		} finally {
-			set({
-				loading: false,
-			});
-		}
-	},
+      throw err;
+    } finally {
+      set({
+        loading: false,
+      });
+    }
+  },
 
-	getMe: async () => {
-		try {
-			set({
-				error: null,
-				loading: true,
-			});
+  logout: async () => {
+    try {
+      set({
+        error: null,
+        loading: true,
+      });
 
-			const { data } = await api.get("/me");
+      await api.post("/logout");
 
-			set({
-				user: data,
-			});
-		} catch (err) {
-			const error = err as AxiosError;
+      set({
+        user: null,
+      });
 
-			if (error.response?.status !== 401) {
-				set({
-					error: err,
-				});
-			}
+      toast.success("Logout realizado com sucesso.");
+    } catch (err) {
+      const error = err as AxiosError<{
+        message?: string;
+      }>;
 
-			set({
-				user: null,
-			});
-		} finally {
-			set({
-				loading: false,
-				initialized: true,
-			});
-		}
-	},
+      const message =
+        error?.response?.data?.message || "Erro inesperado ao realizar logout.";
+
+      toast.error(message);
+
+      set({
+        error: err,
+      });
+
+      throw err;
+    } finally {
+      set({
+        loading: false,
+      });
+    }
+  },
+
+  getMe: async () => {
+    try {
+      set({
+        error: null,
+        loading: true,
+      });
+
+      const { data } = await api.get("/me");
+
+      set({
+        user: data,
+      });
+    } catch (err) {
+      const error = err as AxiosError;
+
+      if (error.response?.status !== 401) {
+        set({
+          error: err,
+        });
+      }
+
+      set({
+        user: null,
+      });
+    } finally {
+      set({
+        loading: false,
+        initialized: true,
+      });
+    }
+  },
 }));
